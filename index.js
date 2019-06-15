abddffca93ee0c4473984fad528c6f628f00833b
Princess Irabor <irabor.princess1@gmail.com>
2019-06-15 03:24:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(currentList, newName){
-  newNumberIndex = currentList.length
-  newNo = currentList.length +1;
+  var newNumberIndex = currentList.length
+  var newNo = currentList.length +1;
   currentList[newNumberIndex] = newName;
   return "Welcome, "+newName+". You are number "+newNo+" in line."
 }
